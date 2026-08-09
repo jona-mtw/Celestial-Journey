01ae25b8c30732a7c5940310d0540b8d669aa919
jona-mtw <jonathanphilipmathew@gmail.com>
2026-08-10 00:32:10 +0100

    starting-player-model
    commited this early to start on procedural generation

--- src/gameplay/player/player.gd
+++ src/gameplay/player/player.gd
@@ -10,15 +10,12 @@ func _input(event: InputEvent) -> void:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		
-	# Handle jump.
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
 	var input_dir: Vector2 = Input.get_vector("left", "right", "forward", "back")
 	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
